10fa8c5184c3828b6229714e8b86a41e77a4e800
SupurCalvinHiggins <calvin@supurloop.com>
2022-11-08 21:02:57 -0500

    Renamed generator to transfer_matrix to align with the book.

--- benchmark_feed_forward_encoder.py
+++ benchmark_feed_forward_encoder.py
@@ -6,11 +6,11 @@ from feed_forward_encoder import FeedForwardEncoder
 BENCHMARK_SIZE = 10_000_000
 
 
-def make_benchmark(generator):
+def make_benchmark(transfer_matrix):
     def benchmark() -> int:
         np.random.seed(0)
         data = list(np.random.choice([0, 1], size=BENCHMARK_SIZE))
-        encoder = FeedForwardEncoder(generator=generator)
+        encoder = FeedForwardEncoder(transfer_matrix=transfer_matrix)
         start_time = time.time()
         _ = encoder.encode(data)
         end_time = time.time()
@@ -20,7 +20,7 @@ def make_benchmark(generator):
 
 
 def benchmark_all() -> None:
-    generators = [
+    transfer_matrices = [
         [[1, 0], [1, 1]],
         [[1, 0, 0], [1, 0, 1], [1, 1, 1]],
         [
@@ -31,8 +31,10 @@ def benchmark_all() -> None:
             [1, 0, 1, 0, 0, 0],
         ],
     ]
-    for generator in generators:
-        print(f"Benchmark {generator}: {make_benchmark(generator)()} seconds")
+    for transfer_matrix in transfer_matrices:
+        print(
+            f"Benchmark {transfer_matrix}: {make_benchmark(transfer_matrix)()} seconds"
+        )
 
 
 if __name__ == "__main__":
--- feed_forward_encoder.py
+++ feed_forward_encoder.py
@@ -2,21 +2,24 @@ import numpy as np
 
 
 class FeedForwardEncoder:
-    def __init__(self, generator):
-        self._generator = generator
-        self._generator_degree = len(generator[0])
-        self._padding = np.array([0] * (self._generator_degree - 1))
-        assert all(map(lambda x: len(x) == self._generator_degree, self._generator))
+    def __init__(self, transfer_matrix: list[list[int]]) -> None:
+
+        self._transfer_matrix = transfer_matrix
+        self._transfer_matrix_degree = len(transfer_matrix[0])
+        self._padding = np.array([0] * (self._transfer_matrix_degree - 1))
+        assert all(
+            map(lambda x: len(x) == self._transfer_matrix_degree, self._transfer_matrix)
+        )
 
     def encode(self, data):
         input_data = np.append(data, self._padding)
-        output_data = np.zeros(input_data.size * len(self._generator))
+        output_data = np.zeros(input_data.size * len(self._transfer_matrix))
 
-        for poly_bits in zip(*self._generator):
+        for poly_bits in zip(*self._transfer_matrix):
             for i, poly_bit in enumerate(poly_bits):
                 if poly_bit:
-                    output_data[i :: len(self._generator)] = np.logical_xor(
-                        output_data[i :: len(self._generator)],
+                    output_data[i :: len(self._transfer_matrix)] = np.logical_xor(
+                        output_data[i :: len(self._transfer_matrix)],
                         input_data,
                     )
             input_data = np.roll(input_data, 1)
