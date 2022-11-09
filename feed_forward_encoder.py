12c181efc99c2e2ea5d8affdb84ab04b932adf36
SupurCalvinHiggins <calvin@supurloop.com>
2022-11-08 21:18:48 -0500

    Added docstring.

--- feed_forward_encoder.py
+++ feed_forward_encoder.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class FeedForwardEncoder:
     def __init__(self, transfer_matrix: list[list[int]]) -> None:
-        """Construct a feedforward (FIR) convolutional encoder from a given transfer
+        """Constructs a feedforward (FIR) convolutional encoder from a given transfer
         function matrix with polynomial entries in F[[x]]^2. See Chapter 12 of
         Todd Moon's Error Correction Coding: Mathematical Methods and Algorithms for
         additional details.
@@ -17,14 +17,26 @@ class FeedForwardEncoder:
                 must be the same, i.e., leading zero coefficients must be provided.
         """
         self._transfer_matrix = transfer_matrix
-        self._transfer_matrix_degree = len(transfer_matrix[0])
-        self._padding = np.array([0] * (self._transfer_matrix_degree - 1))
+        self._transfer_matrix_degree = len(transfer_matrix[0]) - 1
+        self._padding = np.array([0] * self._transfer_matrix_degree)
         assert all(
-            map(lambda x: len(x) == self._transfer_matrix_degree, self._transfer_matrix)
+            map(
+                lambda x: len(x) == (self._transfer_matrix_degree + 1),
+                self._transfer_matrix,
+            )
         )
 
     def encode(self, data: np.ndarray) -> np.ndarray:
-        """ """
+        """Encodes an array of bits using the feedforward convolutional encoder.
+        Additional zero bits equivalent to the maximum degree of the entries of
+        transfer function matrix will be appended to the data prior to encoding to
+        ensure proper termination of the encoder.
+
+        Args:
+            data: Array of bits to encode with the feedforward convolutional encoder.
+
+        Return: The array of bits after encoding.
+        """
         input_data = np.append(data, self._padding)
         output_data = np.zeros(input_data.size * len(self._transfer_matrix))
 
