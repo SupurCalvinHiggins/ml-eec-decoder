acd0e2c9ff6fd40da82fe40f85d5d084f958cf96
SupurCalvinHiggins <calvin@supurloop.com>
2022-11-12 13:06:04 -0500

    Improved BSC code.

--- binary_symmetric_channel.py
+++ binary_symmetric_channel.py
@@ -4,12 +4,12 @@ import numpy as np
 class BinarySymmetricChannel:
     def __init__(self, crossover_probability: float) -> None:
         assert 0 <= crossover_probability <= 1
-        self._crossover_probability = crossover_probability
+        self._distribution = [1 - crossover_probability, crossover_probability]
+        self._sample_space = np.array([0, 1], dtype=np.bool8)
 
     def transform(self, data: np.ndarray) -> np.ndarray:
+        data = data.astype(np.bool8, copy=False)
         noise = np.random.choice(
-            [0, 1],
-            size=data.size,
-            p=[1 - self._crossover_probability, self._crossover_probability],
+            self._sample_space, size=data.size, p=self._distribution
         )
-        return np.logical_xor(data, noise)
+        return np.logical_xor(data, noise, dtype=np.bool8)
