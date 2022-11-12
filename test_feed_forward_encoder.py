082815356efffe63dad03a162dce9626f51e2e60
SupurCalvinHiggins <calvin@supurloop.com>
2022-11-12 13:07:40 -0500

    Renamed encode to transform.

--- test_feed_forward_encoder.py
+++ test_feed_forward_encoder.py
@@ -5,12 +5,12 @@ from feed_forward_encoder import FeedForwardEncoder
 def test_feedforward_encoder_1() -> None:
     encoder = FeedForwardEncoder(transfer_matrix=[[1]])
     input_data = [1, 1, 0, 0, 1, 0]
-    output_data = encoder.encode(input_data)
+    output_data = encoder.transform(input_data)
     assert np.array_equal(output_data, input_data)
 
 
 def test_feedforward_encoder_10_11() -> None:
     encoder = FeedForwardEncoder(transfer_matrix=[[1, 0], [1, 1]])
     input_data = [1, 1, 0, 0, 1, 0]
-    output_data = encoder.encode(input_data)
+    output_data = encoder.transform(input_data)
     assert np.array_equal(output_data, [1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0])
