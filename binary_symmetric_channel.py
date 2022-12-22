2c51e9b18479dce72f93fec962ecbca542667d92
SupurCalvinHiggins <calvin@supurloop.com>
2022-12-21 21:13:52 -0500

    Added comments to BSC

--- binary_symmetric_channel.py
+++ binary_symmetric_channel.py
@@ -3,13 +3,34 @@ import numpy as np
 
 class BinarySymmetricChannel:
     def __init__(self, crossover_probability: float) -> None:
+        """Construct a binary symmetric channel. This channel introduces single bit
+        errors with crossover_probability probability.
+
+        Args:
+            crossover_probability:
+                The probabilty of a single bit error.
+        """
+
         assert 0 <= crossover_probability <= 1
+
         self._distribution = [1 - crossover_probability, crossover_probability]
-        self._sample_space = np.array([0, 1], dtype=np.bool8)
 
     def transform(self, data: np.ndarray) -> np.ndarray:
-        data = data.astype(np.bool8, copy=False)
-        noise = np.random.choice(
-            self._sample_space, size=data.size, p=self._distribution
-        )
-        return np.logical_xor(data, noise, dtype=np.bool8)
+        """Introduce bit errors into the data.
+
+        Args:
+            data:
+                The array of bits to introduce bit errors to. The behavior of this
+                method is undefined on arrays that contain elements other than 0 and 1.
+
+        Return: The data with bit errors.
+        """
+
+        # Convert the data to the appropriate data type.
+        data = data.astype(np.uint8, copy=False)
+
+        # Generate noise.
+        noise = np.random.choice((0, 1), size=data.size, p=self._distribution)
+
+        # Introduce noise into the data.
+        return np.logical_xor(data, noise, dtype=np.uint8)
